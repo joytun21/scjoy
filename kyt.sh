608d6136f35405686c435fb1c95b8c21b9173964
joytun21 <istiqwamancikalang@gmail.com>
2025-01-03 14:58:06 +0700

    Update kyt.sh

--- kyt.sh
+++ kyt.sh
@@ -13,13 +13,13 @@ NC='\e[0m'
 apt update && apt upgrade
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/joytun21/scjoy/main/bot/bot.zip
+wget https://raw.githubusercontent.com/joytun21/scjoy/main/ssh/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget -q -O kyt.zip "https://raw.githubusercontent.com/joytun21/scjoy/main/bot/bot-panel.zip"
+wget -q -O kyt.zip "https://raw.githubusercontent.com/joytun21/scjoy/main/ssh/bot-panel.zip"
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
